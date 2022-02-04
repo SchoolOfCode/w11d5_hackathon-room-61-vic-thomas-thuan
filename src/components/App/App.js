ec1bb628150bfa18e0d07e663ca30023d004a915
thuannguyen88 <getaholdofthuan@gmail.com>
2022-02-04 14:34:40 +0000

    fixed login bug and created logout functionnality

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,12 +3,14 @@ import { Avatar } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import SheepCounter from "../SheepCounter/index";
 import LoginButton from "../LoginButton/LoginButton";
+import LogoutButton from "../LogoutButton/LogoutButton";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <LoginButton />
+        <LogoutButton />
         <Avatar size={64} shape={"circle"} icon={<UserOutlined />} />
         <p> Count sheep before bed </p>
         <SheepCounter />

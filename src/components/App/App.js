fb5d91ce20ce32cf68012ad9d986089a01ee7e7a
Victoria Khatian <khatian.v@gmail.com>
2022-02-04 14:03:17 +0000

    authorisation added

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import { Avatar } from "antd";
 import { UserOutlined } from "@ant-design/icons";
 import SheepCounter from "../SheepCounter/index";
+import LoginButton from "../LoginButton/LoginButton";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <LoginButton />
         <Avatar size={64} shape={"circle"} icon={<UserOutlined />} />
         <p> Count sheep before bed </p>
         <SheepCounter />
--- src/index.js
+++ src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./components/App/App";
+import { Auth0Provider } from "@auth0/auth0-react";
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain="dev-ajod7r2f.us.auth0.com"
+    clientId="TsWI5MbBZc9eqewxvu3Gl7koOO4FDeZb"
+    redirectUri={window.location.origin}
+  >
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
   document.getElementById("root")
 );
